1de452511c3b163a80a6c5023354a41a8005bfbc
pere <pere@soy.yo>
2020-12-10 12:16:00 +0100

    small changes

--- src/index.js
+++ src/index.js
@@ -15,7 +15,10 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    center: true,
+    resizable: false,
+    fullscreenable: false
   });
 
   // and load the index.html of the app.
